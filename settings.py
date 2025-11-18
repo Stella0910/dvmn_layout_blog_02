0fb25020e0ec5296a5139ae6dfed46bce85be13e
Stella <aniretake0910@gmail.com>
2025-11-18 14:09:42 +0300

    Add index page layout

---
+++ settings.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+directory_path = Path.cwd()
+
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     directory_path / 'static/',
+    # directory_path / 'static/img',
+#     # 'D:/Courses/Programming/Devman/LayoutForPythonDeveloper/blog-backend/static/',
+#     # '/home/site/static/',
+#     # '/home/site/project/markup/',
+#     # '/home/site/common/css/',
+# ]
+
+# print(STATICFILES_DIRS)
